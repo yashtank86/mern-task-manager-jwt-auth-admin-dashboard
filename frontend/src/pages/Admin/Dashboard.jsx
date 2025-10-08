235521f24bb0952cf735643db20fa5af5b397924
YT <tankyash86@gmail.com>
2025-10-08 15:49:23 +0530

    change greeting to username

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -77,7 +77,7 @@ const Dashboard = () => {
       <div className="card my-5">
         <div>
           <div className="col-span-3">
-            <h2 className="text-xl md:text-2xl">Good Morning! {user?.name} </h2>
+            <h2 className="text-xl md:text-2xl">Welcome Back! {user?.name} </h2>
             <p className="text-xs md:text-[13px] text-gray-400 mt-1.5">
               {moment().format("dddd Do MMM YYYY")}
             </p>
